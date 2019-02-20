ddc6b99e8fff733caa76a70a058afb7d53d9437c
Anthony Beaumont <dev.xan105@outlook.be>
2019-02-20 17:29:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 module.exports = (filePath) => { 
 
@@ -11,6 +12,15 @@ module.exports = (filePath) => {
   };
 
   return new Promise((resolve, reject) => {
+  
+    try { 
+      if (path.parse(filePath).ext !== ".accountpicture-ms") {
+        return reject("Not an .accountpicture-ms file !");
+      }
+    }catch(err) {
+      return reject(err);
+    }
+  
     fs.readFile(filePath,function(err,data){
       if (err) {
         return reject(err);
